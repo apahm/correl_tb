0f16664b56b401a150024dcfbcf219b1c6b37356
Alex <alexpahmutow@gmail.com>
2023-02-03 18:30:42 +0300

    sdsd

--- src/main.cpp
+++ src/main.cpp
@@ -595,10 +595,8 @@ int parse_ila_data()
     return 0;
 }
 
-int MakeMSequenceIQRecv(uint32_t max_degree)
+int MakeMSequenceIQRecv(uint32_t max_degree, std::vector<float> &data_re, std::vector<float> &data_im)
 {
-    std::vector<float> data_re;
-    std::vector<float> data_im;
 
     std::ofstream m_seq_re_recv("C:/lab_fpga/Synopsis/data/third-party/m_seq_re_recv.dat", std::ios_base::out | std::ios_base::binary);
     std::ofstream m_seq_im_recv("C:/lab_fpga/Synopsis/data/third-party/m_seq_im_recv.dat", std::ios_base::out | std::ios_base::binary);
@@ -658,11 +656,8 @@ int MakeMSequenceIQRecv(uint32_t max_degree)
     return 0;
 }
 
-int MakeMSequenceIQSuppFunc(uint32_t max_degree)
+int MakeMSequenceIQSuppFunc(uint32_t max_degree, std::vector<float> &data_re, std::vector<float> &data_im)
 {
-    std::vector<float> data_re;
-    std::vector<float> data_im;
-
     std::ofstream m_seq_re_sf("C:/lab_fpga/Synopsis/data/third-party/m_seq_re_sf.dat", std::ios_base::out | std::ios_base::binary);
     std::ofstream m_seq_im_sf("C:/lab_fpga/Synopsis/data/third-party/m_seq_im_sf.dat", std::ios_base::out | std::ios_base::binary);
 
@@ -719,9 +714,78 @@ int main(int argc, char const *argv[])
 {
     uint32_t max_degree = 8;
 
-    MakeMSequenceIQRecv(max_degree);
+    std::vector<float> data_re_recv;
+    std::vector<float> data_im_recv;
+    std::vector<float> data_re_sf;
+    std::vector<float> data_im_sf;
 
-    MakeMSequenceIQSuppFunc(max_degree);
+    MakeMSequenceIQRecv(max_degree, data_re_recv, data_im_recv);
+
+    MakeMSequenceIQSuppFunc(max_degree, data_re_sf, data_im_sf);
+
+    int size = 11;
+
+    Ipp32fc* data = nullptr;
+    Ipp32fc* data_shift = nullptr;
+    Ipp32fc* m_bufferFFTRange = nullptr;
+
+    int sizeDFTSpec = 0;
+    int sizeDFTInitBuf = 0;
+    int sizeDFTWorkBuf = 0;
+
+    int fft_len = std::pow(2, size);
+
+    Ipp8u* pDFTSpec = nullptr;
+    Ipp8u* pDFTInitBuf = nullptr;
+    Ipp8u* pDFTWorkBuf = nullptr;
+    IppsFFTSpec_C_32fc* pSpec = 0;
+
+    // IPP_NODIV_BY_ANY
+    ippsFFTGetSize_C_32fc(size, /*IPP_NODIV_BY_ANY*/IPP_NODIV_BY_ANY, ippAlgHintAccurate, &sizeDFTSpec, &sizeDFTInitBuf, &sizeDFTWorkBuf);
+
+    pDFTSpec = ippsMalloc_8u(sizeDFTSpec);
+    pDFTInitBuf = ippsMalloc_8u(sizeDFTInitBuf);
+    pDFTWorkBuf = ippsMalloc_8u(sizeDFTWorkBuf);
+
+    ippsFFTInit_C_32fc(&pSpec, size,/*IPP_NODIV_BY_ANY*/ IPP_NODIV_BY_ANY, ippAlgHintAccurate, pDFTSpec, pDFTInitBuf);
+
+    data = ippsMalloc_32fc(fft_len);
+    data_shift = ippsMalloc_32fc(fft_len);
+
+    ippsZero_32fc(data, fft_len);
+    ippsZero_32fc(data_shift, fft_len);
+
+    for (size_t i = 0; i < fft_len; i++)
+    {
+        data[i].re = data_re_sf[i];
+        data[i].im = data_im_sf[i];
+        
+        data_shift[i].re = data_re_recv[i];
+        data_shift[i].im = data_im_recv[i];
+    }
+
+    add_normal_distribution(data_shift, fft_len, -10, 1);
+
+    ippsFFTFwd_CToC_32fc(data, data, pSpec, pDFTWorkBuf);
+
+    ippsFFTFwd_CToC_32fc(data_shift, data_shift, pSpec, pDFTWorkBuf);
+
+    Ipp32fc* correl = nullptr;
+
+    correl = ippsMalloc_32fc(fft_len);
+
+    ippsZero_32fc(correl, fft_len);
+
+    ippsConj_32fc(data, data, fft_len);
+
+    ippsMul_32fc(data, data_shift, correl, fft_len);
+
+    ippsFFTInv_CToC_32fc(correl, correl, pSpec, pDFTWorkBuf);
+
+    ippsFree(data);
+    ippsFree(pDFTSpec);
+    ippsFree(pDFTInitBuf);
+    ippsFree(pDFTWorkBuf);
 
 	return 0;
 }
